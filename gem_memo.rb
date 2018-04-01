6a960a88bc13f00ac6e59c1539f91a3a89c7573a
simohakki <simohakki@gmail.com>
2018-04-01 17:14:52 +0200

    Adding bundle-exec section

--- gem_memo.rb
+++ gem_memo.rb
@@ -5,3 +5,12 @@
 
 gem uninstall [gem_name]
 gem install  [gem_name]
+
+
+
+#+----------------------------------------------+
+#|               bundle exec                    |
+#+----------------------------------------------+
+
+# Execute a command in the context of the bundle
+bundle-exec
